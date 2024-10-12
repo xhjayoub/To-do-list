902e75f2f0f92a90c040358fcdedab4561470758
Ayoub hj <medayoubhajamor197@gmail.com>
2024-10-12 14:16:16 +0100

    Generate and append the two sides main/tasks

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 // index.js
 import "./styles.css";
 import  { DOM } from "./dom.js";
+
+// Load MainSide
+const mainSide = DOM.makeElement("div");
+DOM.addClass(mainSide, "mainSide");
+
+// Load TaskSide
+const taskSide = DOM.makeElement("div");
+DOM.addClass(taskSide,"taskSide");
+
+
+DOM.appendElement(mainSide,"body");
+DOM.appendElement(taskSide,"body");
