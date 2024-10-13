263fc527960536a6e628c6224f144aa176502021
Ayoub hj <medayoubhajamor197@gmail.com>
2024-10-13 12:43:10 +0100

    Refactor the code Put it inside functions

--- src/index.js
+++ src/index.js
@@ -2,31 +2,36 @@ import { DOM } from "./dom.js";
 import { generateProjects } from "./generate.js";
 import "./styles.css";
 import { projDialog } from "./elements.js";
-// Main side
-const mainSide = new DOM("div");
-mainSide.addClass("mainSide");
+function InitializeMainSide() {
+    // Main side
+    const mainSide = new DOM("div");
+    mainSide.addClass("mainSide");
 
-// Main header
-const mainHeader = new DOM("div");
-mainHeader.addClass("mainHeader");
-const mainTitle = new DOM("div","My Projects", "mainTitle");
-const addProject = new DOM("div","+","addProject");
+    // Main header
+    const mainHeader = new DOM("div");
+    mainHeader.addClass("mainHeader");
+    const mainTitle = new DOM("div", "My Projects", "mainTitle");
+    const addProject = new DOM("div", "+", "addProject");
 
-mainHeader.appendChilds(mainTitle.element, addProject.element);
-// Projects
-const projects = new DOM("div");
-projects.addClass("projects");
-const defaultProjects = ["Home","Workout","Hobbies"];
-generateProjects(defaultProjects,projects);
+    mainHeader.appendChilds(mainTitle.element, addProject.element);
+    // Projects
+    const projects = new DOM("div");
+    projects.addClass("projects");
+    const defaultProjects = ["Home", "Workout", "Hobbies"];
+    generateProjects(defaultProjects, projects);
 
-mainSide.appendChilds(mainHeader.element, projects.element);
-mainSide.appendThisToBody();
+    mainSide.appendChilds(mainHeader.element, projects.element);
+    mainSide.appendThisToBody();
+}
+
+function InitializeTaskSide() {
+    // Tasks side
+    const taskSide = new DOM("div");
+    taskSide.addClass("taskSide");
+
+    taskSide.appendThisToBody();
+
+    projDialog.appendThisToBody();
+}
 
 
-// Tasks side
-const taskSide = new DOM("div");
-taskSide.addClass("taskSide");
-
-taskSide.appendThisToBody();
-
-projDialog.appendThisToBody();
