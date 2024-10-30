a48fa50d12dfa8ff214e0ed18881d5da8296561b
Ayoub hj <medayoubhajamor197@gmail.com>
2024-10-30 19:49:32 +0100

    Add basic Task class

--- src/data.js
+++ src/data.js
@@ -1,5 +1,14 @@
 export { projects };
 
+class Task {
+    constructor(name, description, dueDate, priority) {
+        this.name = name;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+    } 
+}
+
 let projects = {
     Home:[],
     Work:[],
